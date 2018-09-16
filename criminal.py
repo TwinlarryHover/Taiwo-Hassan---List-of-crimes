4d43ebf272230845dc68dff3b16ae33709a9dda9
TwinlarryHover <42385641+TwinlarryHover@users.noreply.github.com>
2018-09-16 17:01:31 +0100

    Add files via upload

---
+++ criminal.py
@@ -0,0 +1,3 @@
+#Criminal profiling 
+criminals = {'property crime':'asset','white collar crime':'elites of society', 'blue collar crime':'wretched of earth', 'crime against state':'treason and secession', 'hate crime':'islamophobia',  'violent crime':'rape'}
+print (criminals)
